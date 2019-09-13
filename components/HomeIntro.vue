d45465209af21d8852b54a98ce24ff1ff9297e07
didithilmy <didithilmy@gmail.com>
2019-09-13 10:02:01 +0700

    Fix text background and cursor

--- components/HomeIntro.vue
+++ components/HomeIntro.vue
@@ -4,11 +4,15 @@
         <v-container fill-height fluid class="pa-0">
           <v-layout wrap>        
             <v-layout align-center column fill-height>
-              <v-img :src="require('~/assets/intro.svg')" width="100%" style="max-width: 900px; margin: 30px">
+              <v-img :src="require('~/assets/intro.svg')" width="100%" class="bgimg ma-5">
                 <v-layout align-center justify-center column fill-height>
-                  <p id="typed" class="font-weight-bold text-center display-2" style="background-color: #31e8b4;"></p>
+                  <div class="text-center">
+                    <p class="text-center display-2 main-heading">
+                      <span class="font-weight-bold" id="typed"></span>
+                    </p>
+                  </div>
                   <!-- <div style="background-color: #e83158; display: flex; justify-content: center; align-items: center;"> -->
-                    <p class="text-center body-1" style="color: white; background-color: #e83158; padding: 10px">Transforming soon, February 2020.</p>
+                    <p class="text-center body-1 mt-3" style="color: white; background-color: #e83158; padding: 10px">Transforming soon, February 2020.</p>
                   <!-- </div> -->
                   <v-btn rounded class="text-capitalize" color="white">I'm interested!</v-btn>
                 </v-layout>
@@ -28,7 +32,8 @@
     mounted() {
       new Typed('#typed', {
         strings: ['It\'s time for you to embrace the nation.', 'Arkavidia 6.0', 'Coming soon...'],
-        typeSpeed: 50,
+        typeSpeed: 60,
+        backDelay: 1000,
         loop: true
       });
     }
@@ -36,4 +41,14 @@
 </script>
 
 <style scoped>
+  .bgimg {
+    max-width: 900px;
+  }
+  .main-heading {
+    background-color: #31e8b4;
+    display: inline;
+    line-height: 1.5;
+    padding: 4px 8px 4px 8px;
+    box-decoration-break: clone;
+  }
 </style>
