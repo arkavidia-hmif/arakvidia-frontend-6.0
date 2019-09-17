d69779f3902e00558df9f9b1cffb47153861d1a5
didithilmy <didithilmy@gmail.com>
2019-09-17 09:54:43 +0700

    Change font size

--- components/competition/hackathon/HackathonIntro.vue
+++ components/competition/hackathon/HackathonIntro.vue
@@ -8,17 +8,18 @@
 						<p class="mt-4 grey--text text--darken-1">
 							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
 						</p>
-						<div class="mt-4">
+						<div class="mt-8 caption">
 							Kategori
 							&nbsp;
-							<v-chip color="black" class="black white--text">Mahasiswa</v-chip>
+							<v-chip small color="black" class="black white--text caption">Mahasiswa</v-chip>
 						</div>
 						<div class="mt-8">
 							<v-btn large rounded outlined color="#0B909A" class="text-none mr-2 mt-2">Download guidebook</v-btn>
 							<v-btn large rounded color="primary" class="text-none mt-2" elevation="0">Daftar sekarang</v-btn>
 						</div>
 					</v-col>
-					<v-col :cols="12" :md="4" :offset-md="1" align="center" class="hidden-sm-and-down" align-self="center">
+					<v-col :cols="12" :lg="3" :offset-lg="2" :md="4" :offset-md="1" align="center" class="hidden-sm-and-down"
+								 align-self="center">
 						<v-img width="100%" fill-height cover :src="require('~/assets/hackathon_intro.jpg')"></v-img>
 					</v-col>
 				</v-row>
