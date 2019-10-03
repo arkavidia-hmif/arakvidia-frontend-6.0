f9c3c471befeec7cfdc7dad344ed46ffa851cfd3
Nella Zabrina <nellazabrinapramata@yahoo.co.id>
2019-10-03 11:57:19 +0700

    hide faq

--- pages/competition/Hackavidia.vue
+++ pages/competition/Hackavidia.vue
@@ -4,7 +4,7 @@
       <HackathonIntro />
       <HackathonPrize />
       <HackathonTimeline />
-      <CompetitionFAQ :items="faqItems" />
+      <!-- <CompetitionFAQ :items="faqItems" /> -->
     </v-flex>
   </v-layout>
 </template>
@@ -27,15 +27,15 @@ export default Vue.extend({
     return {
       faqItems: [
         {
-          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elita?',
           answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         {
-          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite?',
           answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         {
-          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing eliti?',
           answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         }
       ]
