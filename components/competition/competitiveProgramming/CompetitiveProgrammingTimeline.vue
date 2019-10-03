ace91d35fd557ade235651b96258206f17c641c0
bmusuko <bram.musuko@gmail.com>
2019-10-03 16:25:48 +0700

    Competitive Programming Page: intro

---
+++ components/competition/competitiveProgramming/CompetitiveProgrammingTimeline.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container px-4>
+    <div class="headline font-weight-bold">
+      Timeline Kegiatan
+    </div>
+    <v-row class="mt-4" align="stretch">
+      <v-col :cols="6" :sm="3">
+        <TimelineItem
+          active
+          date="08"
+          month-year="September 2019"
+          title="Pembukaan Pendaftaran"
+          description="Pendaftaran dilakukan secara online"
+        />
+      </v-col>
+      <v-col :cols="6" :sm="3">
+        <TimelineItem
+          date="30"
+          month-year="Oktober 2019"
+          title="Pembukaan Pendaftaran"
+          description="Pendaftaran dilakukan secara online"
+        />
+      </v-col>
+      <v-col :cols="6" :sm="3">
+        <TimelineItem
+          date="30"
+          month-year="Oktober 2019"
+          title="Pembukaan Pendaftaran"
+          description="Pendaftaran dilakukan secara online"
+        />
+      </v-col>
+      <v-col :cols="6" :sm="3">
+        <TimelineItem
+          date="30"
+          month-year="Oktober 2019"
+          title="Pembukaan Pendaftaran"
+          description="Pendaftaran dilakukan secara online"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TimelineItem from '~/components/competition/TimelineItem.vue'
+
+export default Vue.extend({
+  name: 'CompetitiveProgrammingTimeline',
+  components: { TimelineItem }
+})
+</script>
+
+<style scoped>
+</style>
---
+++ pages/competition/competitive-programming.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container
+    fluid
+    align-start
+    column
+    class="pa-0 ma-0"
+    style="background: #FFFFFF"
+  >
+    <v-layout column>
+      <v-flex>
+        <CompetitiveProgrammingIntro />
+        <CompetitiveProgrammingPrize />
+        <CompetitiveProgrammingTimeline />
+        <CompetitionFAQ :items="faqItems" />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import CompetitiveProgrammingIntro from '~/components/competition/competitiveProgramming/CompetitiveProgrammingIntro.vue'
+import CompetitiveProgrammingPrize from '~/components/competition/competitiveProgramming/CompetitiveProgrammingPrize.vue'
+import CompetitiveProgrammingTimeline from '~/components/competition/competitiveProgramming/CompetitiveProgrammingTimeline.vue'
+import CompetitionFAQ from '~/components/competition/CompetitionFAQ.vue'
+
+export default Vue.extend({
+  components: {
+    CompetitiveProgrammingIntro,
+    CompetitiveProgrammingPrize,
+    CompetitiveProgrammingTimeline,
+    CompetitionFAQ
+  },
+  data () {
+    return {
+      faqItems: [
+        {
+          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+          answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+          answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+
+        {
+          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+          answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }
+      ]
+    }
+  }
+})
+</script>
