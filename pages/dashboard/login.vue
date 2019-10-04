052fb96258c3ad2467e9bdd4f2e2b60ec73a44c3
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-10-04 13:21:30 +0700

    Update vuetify, add login page, disable Sentry on development

--- pages/dashboard/login.vue
+++ pages/dashboard/login.vue
@@ -1,5 +1,33 @@
 <template>
-  <b>Dashboard login</b>
+  <v-container fill-height class="mt-12">
+    <v-row class="mt-8 mb-12">
+      <v-col :cols="12" :md="4">
+        <div class="headline font-weight-bold section-title">
+          Login ke Dashboard
+        </div>
+        <v-text-field
+          class="mt-8"
+          label="Alamat E-mail"
+          outlined
+          dense
+        />
+        <v-text-field
+          label="Kata sandi"
+          type="password"
+          outlined
+          dense
+        />
+        <v-btn class="mt-1 text-none" block outlined>
+          Login
+        </v-btn>
+        <div class="mt-2">
+          <nuxt-link to="/dashboard/recover" style="text-decoration: none">
+            Lupa kata sandi?
+          </nuxt-link>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -15,5 +43,18 @@ export default Vue.extend({
 </script>
 
 <style scoped>
+  .section-title {
+    position: relative;
+    padding-bottom: 8px;
+  }
 
+  .section-title:after {
+    content: "";
+    position: absolute;
+    width: 100px;
+    height: 1px;
+    bottom: 0;
+    left: 0;
+    border-bottom: 4px solid #FF084F;
+  }
 </style>
