e6150d59b9bf0c2545e1036b67b66601bd300305
alfinm01 <arrestral@gmail.com>
2019-10-06 02:21:29 +0700

    Add Arkavidia Academy page

---
+++ components/preevent/academy/Intro.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container fill-height py-0 px-4 class="d-flex flex-column" style="min-height: 100vh">
+    <div class="d-flex flex-column" style="flex: 1">
+      <v-container class="d-flex mt-12" style="flex: 1">
+        <v-row wrap>
+          <v-col
+            :cols="12"
+            :md="6"
+            align="center"
+            align-self="center"
+          >
+            <v-img width="100%" fill-height cover :src="require('~/assets/images/preevent/dummy_asset.JPG')" :style="$vuetify.breakpoint.mdAndUp ? 'max-width: 70vh' : 'max-width: 50vh'" />
+          </v-col>
+          <v-col :cols="12" :md="6" align-self="center">
+            <div class="display-1 font-weight-bold" style="color: #0B909A">
+              Arkavidia Academy
+            </div>
+            <p class="mt-4 mr-5 grey--text text--darken-1">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+            </p>
+            <div class="mt-8">
+              <v-btn large rounded color="primary" class="text-none mt-2" elevation="0">
+                Daftar sekarang
+              </v-btn>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+      <div align="center" class="pa-6">
+        <div class="font-weight-bold" style="color: #0435B5">
+          Penasaran?
+        </div>
+        <v-img :src="require('~/assets/down_arrow.svg')" width="20" class="mt-2 vert-move" />
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AcademyIntro'
+});
+</script>
+
+<style scoped>
+  .vert-move {
+    -webkit-animation: mover .5s infinite alternate;
+    animation: mover .5s infinite alternate;
+  }
+
+  @-webkit-keyframes mover {
+    0% {
+      transform: translateY(-3px);
+    }
+    100% {
+      transform: translateY(7px);
+    }
+  }
+
+  @keyframes mover {
+    0% {
+      transform: translateY(-3px);
+    }
+    100% {
+      transform: translateY(7px);
+    }
+  }
+
+</style>
---
+++ pages/preevent/arkavidia-academy.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-layout column style="background: white;">
+    <v-flex>
+      <AcademyIntro />
+      <AcademyAbout />
+      <AcademyTimeline />
+      <FAQ :items="faqItems" />
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import Vue, { ComponentOptions } from 'vue';
+import AcademyIntro from '~/components/preevent/academy/Intro.vue';
+import AcademyAbout from '~/components/preevent/academy/About.vue';
+import AcademyTimeline from '~/components/preevent/academy/Timeline.vue';
+import FAQ from '~/components/partials/FAQ.vue'
+
+export default Vue.extend({
+  components: {
+    AcademyIntro,
+    AcademyAbout,
+    AcademyTimeline,
+    FAQ
+  },
+  data() {
+    return {
+      faqItems: [
+        {
+          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elita?',
+          answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite?',
+          answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+          question: 'Lorem ipsum dolor sit amet, consectetur adipiscing eliti?',
+          answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }
+      ]
+    };
+  },
+  head() {
+    return {
+      title: 'Arkavidia Academy'
+    };
+  }
+} as ComponentOptions<Vue>);
+</script>
