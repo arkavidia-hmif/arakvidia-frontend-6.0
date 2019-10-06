a0aa9cbd55def4837e3f0196299915000f9bfccd
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-10-06 16:14:01 +0700

    Refactor Dashboard to use partials

---
+++ components/partials/Dashboard/DashboardWrapper.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container v-if="loggedIn" class="mt-12">
+    <div class="headline font-weight-bold mt-12" style="color: black">
+      Dashboard
+    </div>
+    <div class="display-1 font-weight-bold mt-2 mb-5" style="color: #0B909A">
+      Halo, {{ user.fullName }}!
+    </div>
+    <v-tabs slider-color="#E44D4B" slider-size="5" class="text-transform-none">
+      <v-tab v-for="(item, i) in dashboardMenus" :key="i" :to="item.route" style="text-transform: none;" class="font-weight-bold black--text caption">
+        {{ item.title }}
+      </v-tab>
+    </v-tabs>
+    <v-divider />
+    <div>
+      <slot />
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue, State } from 'nuxt-property-decorator';
+import { dashboardMenus, Menu } from '~/constants/menus';
+import { User } from '~/api/user';
+
+  @Component({
+    name: 'DashboardWrapper'
+  })
+export default class DashboardIndex extends Vue {
+    @State loggedIn!: boolean;
+    @State user!: User;
+    dashboardMenus: Menu[] = dashboardMenus;
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/dashboard/competition.vue
+++ pages/dashboard/competition.vue
@@ -1,31 +1,18 @@
 <template>
-  <v-container v-if="loggedIn" class="mt-12">
-    <div class="headline font-weight-bold mt-12" style="color: black">
-      Dashboard
-    </div>
-    <div class="display-1 font-weight-bold mt-2 mb-5" style="color: #0B909A">
-      Halo, Nella Zabrina!
-    </div>
-    <v-tabs slider-color="#E44D4B" slider-size="5" class="text-transform-none">
-      <v-tab v-for="(item, i) in dashboardMenus" :key="i" :to="''+item.route" style="text-transform: none;" class="font-weight-bold black--text caption">
-        {{ item.title }}
-      </v-tab>
-    </v-tabs>
-    <v-divider />
+  <DashboardWrapper>
     <p class="mt-4 grey--text text--darken-1">
       Competition
     </p>
-  </v-container>
+  </DashboardWrapper>
 </template>
 
 <script lang="ts">
-import { Component, Vue, State } from 'nuxt-property-decorator';
-import { dashboardMenus, Menu } from '../../constants/menus';
-@Component
+import { Component, Vue } from 'nuxt-property-decorator';
+import DashboardWrapper from '~/components/partials/Dashboard/DashboardWrapper';
+@Component({
+  components: { DashboardWrapper }
+})
 export default class DashboardIndex extends Vue {
-  @State loggedIn!: boolean;
-  dashboardMenus: Menu[] = dashboardMenus;
-
   head() {
     return {
       title: 'Dashboard'
