2032cc3f35e200ca0bcc572a4616c4630b67b05d
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-10-06 16:22:53 +0700

    Change text transform

--- components/partials/Dashboard/DashboardWrapper.vue
+++ components/partials/Dashboard/DashboardWrapper.vue
@@ -6,8 +6,8 @@
     <div class="display-1 font-weight-bold mt-2 mb-5" style="color: #0B909A">
       Halo, {{ user.fullName }}!
     </div>
-    <v-tabs slider-color="#E44D4B" slider-size="5" class="text-transform-none">
-      <v-tab v-for="(item, i) in dashboardMenus" :key="i" :to="item.route" style="text-transform: none;" class="font-weight-bold black--text caption">
+    <v-tabs slider-color="#E44D4B" slider-size="5">
+      <v-tab v-for="(item, i) in dashboardMenus" :key="i" :to="item.route" class="font-weight-bold black--text caption text-none">
         {{ item.title }}
       </v-tab>
     </v-tabs>
