1615e498d5f7fc9efb495ce50b03eefab6eb9e65
bmusuko <bram.musuko@gmail.com>
2019-10-16 10:50:52 +0700

    dashborad profile

--- pages/dashboard/profile.vue
+++ pages/dashboard/profile.vue
@@ -1,8 +1,74 @@
 <template>
   <DashboardWrapper>
-    <p class="mt-4 grey--text text--darken-1">
-      Profile
-    </p>
+     <v-layout row wrap class="mt-4">
+
+          <v-flex md3 xs12>
+            <div  class="ml-3 mb-5">
+              <div class="subtitle-2 font-weight-black "style="color: #666666">
+                Name
+              </div>
+              <div class="subtitle-2 black--text">
+                Nella Zabrina Pratama
+              </div>
+            </div>
+
+            <div class="ml-3 mb-5">
+              <div class="subtitle-2 font-weight-black "style="color: #666666">
+                Email
+              </div>
+              <div class="subtitle-2 black--text">
+                nella.zabrina@gmail.com
+              </div>
+            </div>
+
+            <div class="ml-3 mb-5">
+              <div class="subtitle-2 font-weight-black "style="color: #666666">
+                Education
+              </div>
+              <div class="subtitle-2 black--text">
+                Mahasiswa
+              </div>
+            </div>
+
+            <div class="ml-3 mb-5">
+              <div class="subtitle-2 font-weight-black "style="color: #666666">
+                Institution
+              </div>
+              <div class="subtitle-2 black--text">
+                Institut Teknologi Bandung
+              </div>
+            </div>
+
+          </v-flex>
+          <v-flex md6 xs12>
+            <div  class="ml-3 mb-5">
+              <div class="subtitle-2 font-weight-black "style="color: #666666">
+                Phone Number
+              </div>
+              <div class="subtitle-2 black--text">
+                +62812872878572
+              </div>
+            </div>
+            <div  class="ml-3 mb-5">
+              <div class="subtitle-2 font-weight-black "style="color: #666666">
+                Birth Date
+              </div>
+              <div class="subtitle-2 black--text">
+                07 September 1999
+              </div>
+            </div>
+            <div  class="ml-3 mb-5">
+              <div class="subtitle-2 font-weight-black "style="color: #666666">
+                Address
+              </div>
+              <div class="subtitle-2 black--text">
+                Jl. Cisitu Indah 5 no.19B, Dago, Coblong, Kota Bandung
+              </div>
+            </div>
+          </v-flex>
+      </v-layout>
+      <v-btn class="my-5 primary subtitle-2 text-none px-5 font-weight-bold" style="border-radius: 50px">Edit Profile</v-btn>
+    </v-container>
   </DashboardWrapper>
 </template>
 
