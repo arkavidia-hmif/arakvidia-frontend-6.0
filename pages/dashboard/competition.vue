7fb98f94012f634dcc62d4bce7686313e6da0b01
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-10-17 10:41:31 +0700

    Fix lint, refactor CompetitionCard

--- pages/dashboard/competition.vue
+++ pages/dashboard/competition.vue
@@ -1,112 +1,36 @@
 <template>
   <DashboardWrapper>
-    <v-card width="350" class="d-inline-block mx-4 my-5" elevation="3">
-      <v-container>
-        <v-list-item three-line class="px-0">
-          <v-list-item-content>
-            <v-list-item-title class="headline cyan--text text--darken-2">
-              <strong>Competitive Programming</strong>
-            </v-list-item-title>
-            <v-list-item-subtitle>Untuk Mahasiswa</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-        <hr />
-      </v-container>
-
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text class="teal--text text--lighten-1">View Application</v-btn>
-      </v-card-actions>
-    </v-card>
-
-    <v-card width="350" class="d-inline-block mx-4 my-5" elevation="3">
-      <v-container>
-        <v-list-item three-line class="px-0">
-          <v-list-item-content>
-            <v-list-item-title class="headline cyan--text text--darken-2">
-              <strong>Capture The Flag</strong>
-            </v-list-item-title>
-            <v-list-item-subtitle>Untuk SMA & Mahasiswa</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-        <hr />
-      </v-container>
-
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text class="teal--text text--lighten-1">Register</v-btn>
-      </v-card-actions>
-    </v-card>
-
-    <v-card width="350" class="d-inline-block mx-4 my-5" elevation="3">
-      <v-container>
-        <v-list-item three-line class="px-0">
-          <v-list-item-content>
-            <v-list-item-title class="headline cyan--text text--darken-2">
-              <strong>Hackavidia</strong>
-            </v-list-item-title>
-            <v-list-item-subtitle>Untuk Mahasiswa</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-        <hr />
-      </v-container>
-
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text class="teal--text text--lighten-1">Register</v-btn>
-      </v-card-actions>
-    </v-card>
-
-    <v-card width="350" class="d-inline-block mx-4 my-5" elevation="3">
-      <v-container>
-        <v-list-item three-line class="px-0">
-          <v-list-item-content>
-            <v-list-item-title class="headline cyan--text text--darken-2">
-              <strong>Datavidia</strong>
-            </v-list-item-title>
-            <v-list-item-subtitle>Untuk Mahasiswa</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-        <hr />
-      </v-container>
-
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text class="teal--text text--lighten-1">Register</v-btn>
-      </v-card-actions>
-    </v-card>
-
-    <v-card disabled width="350" class="d-inline-block mx-4 my-5" elevation="3">
-      <v-container>
-        <v-list-item three-line class="px-0">
-          <v-list-item-content>
-            <v-list-item-title class="headline cyan--text text--darken-2">
-              <strong>Arkalogica</strong>
-            </v-list-item-title>
-            <v-list-item-subtitle>Untuk SMA</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-        <hr />
-      </v-container>
-
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text>Anda tidak bisa mendaftar lomba ini</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-row class="mt-4">
+      <v-col :cols="12" :lg="4" :sm="6">
+        <CompetitionCard title="Competitive Programming" subtitle="Untuk Mahasiswa" to="/dashboard/competition/competitive-programming" button-title="Lihat Pendaftaran" />
+      </v-col>
+      <v-col :cols="12" :lg="4" :sm="6">
+        <CompetitionCard title="Capture the Flag" subtitle="Untuk Mahasiswa & SMA/Sederajat" to="/dashboard/competition/capture-the-flag" button-title="Daftar" />
+      </v-col>
+      <v-col :cols="12" :lg="4" :sm="6">
+        <CompetitionCard title="Hackavidia" subtitle="Untuk Mahasiswa" to="/dashboard/competition/hackavidia" button-title="Daftar" />
+      </v-col>
+      <v-col :cols="12" :lg="4" :sm="6">
+        <CompetitionCard title="Datavidia" subtitle="Untuk Mahasiswa" to="/dashboard/competition/hackavidia" button-title="Daftar" />
+      </v-col>
+      <v-col :cols="12" :lg="4" :sm="6">
+        <CompetitionCard title="Arkalogica" subtitle="Untuk SMA" to="/dashboard/competition/hackavidia" button-title="Anda tidak dapat mendaftar" :enabled="false" />
+      </v-col>
+    </v-row>
   </DashboardWrapper>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "nuxt-property-decorator";
-import DashboardWrapper from "~/components/partials/Dashboard/DashboardWrapper.vue";
+import { Component, Vue } from 'nuxt-property-decorator';
+import DashboardWrapper from '~/components/partials/Dashboard/DashboardWrapper.vue';
+import CompetitionCard from '~/components/partials/Dashboard/CompetitionCard.vue';
 @Component({
-  components: { DashboardWrapper }
+  components: { DashboardWrapper, CompetitionCard }
 })
 export default class DashboardIndex extends Vue {
   head() {
     return {
-      title: "Dashboard"
+      title: 'Dashboard'
     };
   }
 }
