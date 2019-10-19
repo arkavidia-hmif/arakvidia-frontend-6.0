cc96fed854744791b043e70ae513adeec5c472c6
bmusuko <bram.musuko@gmail.com>
2019-10-19 21:23:53 +0700

    profile component

---
+++ components/partials/Dashboard/ProfileField.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ml-3 mb-5">
+    <div class="subtitle-2 font-weight-black " style="color: #666666">
+      {{ title }}
+    </div>
+    <div class="subtitle-2 black--text">
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'ProfileField',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
--- pages/dashboard/profile.vue
+++ pages/dashboard/profile.vue
@@ -2,67 +2,15 @@
   <DashboardWrapper>
     <v-layout row wrap class="mt-4">
       <v-flex md3 xs12>
-        <div class="ml-3 mb-5">
-          <div class="subtitle-2 font-weight-black " style="color: #666666">
-            Name
-          </div>
-          <div class="subtitle-2 black--text">
-            Nella Zabrina Pratama
-          </div>
-        </div>
-
-        <div class="ml-3 mb-5">
-          <div class="subtitle-2 font-weight-black " style="color: #666666">
-            Email
-          </div>
-          <div class="subtitle-2 black--text">
-            nella.zabrina@gmail.com
-          </div>
-        </div>
-
-        <div class="ml-3 mb-5">
-          <div class="subtitle-2 font-weight-black " style="color: #666666">
-            Education
-          </div>
-          <div class="subtitle-2 black--text">
-            Mahasiswa
-          </div>
-        </div>
-
-        <div class="ml-3 mb-5">
-          <div class="subtitle-2 font-weight-black " style="color: #666666">
-            Institution
-          </div>
-          <div class="subtitle-2 black--text">
-            Institut Teknologi Bandung
-          </div>
-        </div>
+        <ProfileField title="Name" value="Nella Zabrina Pratama" />
+        <ProfileField title="Email" value="nella.zabrina@gmail.com" />
+        <ProfileField title="Education" value="Mahasiswa" />
+        <ProfileField title="Institution" value="nella.zabrina@gmail.com" />
       </v-flex>
       <v-flex md6 xs12>
-        <div class="ml-3 mb-5">
-          <div class="subtitle-2 font-weight-black " style="color: #666666">
-            Phone Number
-          </div>
-          <div class="subtitle-2 black--text">
-            +62812872878572
-          </div>
-        </div>
-        <div class="ml-3 mb-5">
-          <div class="subtitle-2 font-weight-black " style="color: #666666">
-            Birth Date
-          </div>
-          <div class="subtitle-2 black--text">
-            07 September 1999
-          </div>
-        </div>
-        <div class="ml-3 mb-5">
-          <div class="subtitle-2 font-weight-black " style="color: #666666">
-            Address
-          </div>
-          <div class="subtitle-2 black--text">
-            Jl. Cisitu Indah 5 no.19B, Dago, Coblong, Kota Bandung
-          </div>
-        </div>
+        <ProfileField title="Phone Number" value="+62812872878572" />
+        <ProfileField title="Birth Date" value="07 September 1999" />
+        <ProfileField title="Address" value="Jl. Cisitu Indah 5 no.19B, Dago, Coblong, Kota Bandung" />
       </v-flex>
     </v-layout>
     <v-btn class="my-5 primary subtitle-2 text-none px-5 font-weight-bold" style="border-radius: 50px">
@@ -75,8 +23,9 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
 import DashboardWrapper from '~/components/partials/Dashboard/DashboardWrapper.vue';
+import ProfileField from '~/components/partials/Dashboard/ProfileField.vue';
 @Component({
-  components: { DashboardWrapper }
+  components: { DashboardWrapper, ProfileField }
 })
 export default class DashboardIndex extends Vue {
   head() {
