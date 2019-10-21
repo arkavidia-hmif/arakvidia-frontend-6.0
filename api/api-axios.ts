a9f952125d6c1ff56e280c264c9365fbedeb0292
didithilmy <didithilmy@gmail.com>
2019-10-21 14:25:57 +0700

    Fix user API

---
+++ api/api-axios.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
+axios.defaults.withCredentials = true;
+
+export default axios;
--- api/user.ts
+++ api/user.ts
@@ -1,10 +1,20 @@
 // TODO remove eslint disable
 /*eslint-disable */
+import axios from '~/api/api-axios';
 
 export interface User {
   email: String;
   fullName: String;
   userId: String;
+}
+
+export enum RegistrationStatus {
+  SUCCESS, EMAIL_EXISTS, ERROR
+}
+
+export interface RegistrationResult {
+  status: RegistrationStatus,
+  message?: string
 }
 
 export interface AuthenticationResult {
@@ -13,6 +23,10 @@ export interface AuthenticationResult {
 }
 
 export async function login(email: String, password: String): Promise<AuthenticationResult> {
+  const data = { email, password };
+  const response = await axios.post(`${process.env.API_BASE_URL}/api/auth/login/`, data);
+  console.log(response);
+
   return {
     bearerToken: 'jwt-here',
     user: {
@@ -23,12 +37,32 @@ export async function login(email: String, password: String): Promise<Authentica
   };
 }
 
-export async function register(email: String, fullName: String, password: String): Promise<User> {
-  return {
-    email,
-    fullName: 'Ahmad Fahmi Pratama',
-    userId: 'afahmip128d'
-  };
+export async function register(email: String, fullName: String, password: String): Promise<RegistrationResult> {
+  try {
+    const data = {email, password};
+    const response = await axios.post(`${process.env.API_BASE_URL}/api/auth/register/`, data);
+    const code = response.data.error;
+    let status;
+
+    switch (code) {
+      case "registration_successful":
+        status = RegistrationStatus.SUCCESS;
+        break;
+      default:
+        status = RegistrationStatus.EMAIL_EXISTS;
+        break;
+    }
+
+    return {
+      status: status,
+      message: response.data.detail
+    }
+  } catch (e) {
+    throw {
+      status: RegistrationStatus.ERROR,
+      message: e.toString()
+    }
+  }
 }
 
 export async function recover(email: String): Promise<void> {
