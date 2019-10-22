143f89901409b44816afac0906a974dd23378e24
didithilmy <didithilmy@gmail.com>
2019-10-22 11:49:36 +0700

    Hide reg and guidebook link, hide login and register

--- components/competition/arkalogica/Intro.vue
+++ components/competition/arkalogica/Intro.vue
@@ -26,13 +26,23 @@
               </v-chip>
             </div>
             <div class="mt-8">
-              <a href="https://arkav.me/guidebook-arkalogica" style="text-decoration: none;">
-                <v-btn large rounded outlined color="#0B909A" class="text-none mr-2 mt-2">
-                  Download guidebook
-                </v-btn>
-              </a>
-              <v-btn large rounded color="primary" class="text-none mt-2" elevation="0">
-                Daftar sekarang
+              <!--<a href="https://arkav.me/guidebook-arkalogica" style="text-decoration: none;">-->
+              <!--<v-btn large rounded outlined color="#0B909A" class="text-none mr-2 mt-2">-->
+              <!--Download guidebook-->
+              <!--</v-btn>-->
+              <!--</a>-->
+              <!--<v-btn large rounded color="primary" class="text-none mt-2" elevation="0">-->
+              <!--Daftar sekarang-->
+              <!--</v-btn>-->
+              <v-btn
+                large
+                rounded
+                color="primary"
+                class="text-none mt-2"
+                elevation="0"
+                disabled
+              >
+                Segera hadir
               </v-btn>
             </div>
           </v-col>
