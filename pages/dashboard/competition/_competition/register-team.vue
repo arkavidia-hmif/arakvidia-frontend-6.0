caf7466b6159934e3282730c7cd1bf4e0f6e936a
Nella Zabrina <nellazabrinapramata@yahoo.co.id>
2019-10-22 16:18:57 +0700

    fixing page register team

--- pages/dashboard/competition/_competition/register-team.vue
+++ pages/dashboard/competition/_competition/register-team.vue
@@ -3,17 +3,14 @@
     <v-container>
       <div class="px-12 mx-12">
         <h5 class="mt-4 title font-weight-black">
-          Competitive Programming
+          {{ title }}
         </h5>
         <v-text-field v-model="team" label="Team Name" />
         <v-text-field v-model="school" label="Institute / School" />
         <div class="my-2">
-          <v-btn text large color="error">
-            Delete Team
-          </v-btn>
-          <v-btn text large color="primary">
-            Save Team
-          </v-btn>
+        <v-btn class="my-5 primary subtitle-2 text-none px-5 font-weight-bold" style="border-radius: 50px">
+          Save Team
+        </v-btn>
         </div>
       </div>
     </v-container>
@@ -27,16 +24,30 @@ import DashboardWrapper from '~/components/partials/Dashboard/DashboardWrapper.v
   components: { DashboardWrapper }
 })
 export default class Competitive extends Vue {
-  data() {
-    return {
-      team: '',
-      school: ''
-    };
+  team: string = '';
+  school: string = '';
+  id: string = '';
+  title: string = '';
+
+  mounted() {
+    this.id = this.$route.params.competition;
+    var i;
+    var temp = this.id.split("-");
+    for (i = 0; i < temp.length; i++) {
+      this.title += this.jsUcfirst(temp[i]);
+      if (i != temp.length - 1) {
+        this.title += ' ';
+      }
+    }
   }
   head() {
     return {
-      title: 'Pendaftaran CP'
+      title: 'Pendaftaran' + this.title
     };
+  }
+  jsUcfirst(string) 
+  {
+      return string.charAt(0).toUpperCase() + string.slice(1);
   }
 }
 </script>
