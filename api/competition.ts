d0cb5adeb4380d9f4a964c6fe47fa001b5e4191d
Nella Zabrina <nellazabrinapramata@yahoo.co.id>
2019-10-24 00:28:46 +0700

    add register, delete, and get anggota

--- api/competition.ts
+++ api/competition.ts
@@ -3,8 +3,8 @@ import { ApiError, ArkavidiaBaseApi } from '~/api/base';
 export interface Competition {
     id?: number;
     name: string;
-    maxTeamMembers?: string;
-    minTeamMembers?: string;
+    maxTeamMembers?: number;
+    minTeamMembers?: number;
     isRegistrationOpen?: boolean;
 }
 
@@ -31,6 +31,18 @@ export enum RegistrationStatus {
   NAME_EXISTS, ERROR
 }
 
+export enum CreateMemberStatus {
+  EMAIL_EXISTS, ERROR
+}
+
+export enum GetCompetitionStatus {
+  ERROR
+}
+
+export enum DeleteMemberStatus {
+  ERROR
+}
+
 export class ArkavidiaCompetitionApi extends ArkavidiaBaseApi {
   async registerTeam(competitionId: number, name: string, institution: string): Promise<void> {
     try {
@@ -45,6 +57,52 @@ export class ArkavidiaCompetitionApi extends ArkavidiaBaseApi {
       }
 
       throw new ApiError<RegistrationStatus>(RegistrationStatus.ERROR, e.toString());
+    }
+  }
+
+  async registerMember(teamId: number, name: string, email: string): Promise<void> {
+    try {
+      const data = { name, email };
+      await this.axios.post(`/competition/teams/` + teamId + `/members`, data);
+    }
+    catch (e) {
+      if (e.response) {
+        if (e.response.data.code === 'unknown_error') {
+          throw new ApiError<CreateMemberStatus>(CreateMemberStatus.EMAIL_EXISTS, e.response.data.detail);
+        }
+      }
+
+      throw new ApiError<CreateMemberStatus>(CreateMemberStatus.ERROR, e.toString());
+    }
+  }
+
+  async getTeam(teamId: number): Promise<Team> {
+    try {
+      return await this.axios.get(`/competition/teams/` + teamId);
+    }
+    catch (e) {
+      if (e.response) {
+        if (e.response.data.code === 'unknown_error') {
+          throw new ApiError<GetCompetitionStatus>(GetCompetitionStatus.ERROR, e.response.data.detail);
+        }
+      }
+
+      throw new ApiError<GetCompetitionStatus>(GetCompetitionStatus.ERROR, e.toString());
+    }
+  }
+
+  async DeleteMember(teamId: number, team_member_id: number): Promise<void> {
+    try {
+      await this.axios.delete(`/competition/teams/` + teamId + `/members` + team_member_id);
+    }
+    catch (e) {
+      if (e.response) {
+        if (e.response.data.code === 'unknown_error') {
+          throw new ApiError<DeleteMemberStatus>(DeleteMemberStatus.ERROR, e.response.data.detail);
+        }
+      }
+
+      throw new ApiError<DeleteMemberStatus>(DeleteMemberStatus.ERROR, e.toString());
     }
   }
 }
--- components/partials/Dashboard/AnggotaTim.vue
+++ components/partials/Dashboard/AnggotaTim.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ml-3 mb-5">
+  <div v-if="isLoding == false" class="ml-3 mb-5">
     <div>
       <p style="font-weight: 700 !important; margin-bottom: 0 !important;">
         Daftar Anggota
@@ -18,10 +18,10 @@
             </p>
             <v-flex>
               <v-btn
-                :id="'delete-' + i"
+                :memberId="member.id"
                 class="my-5 subtitle-2 text-none px-5 font-weight-bold"
-                style="border-radius: 50px; margin: 0 !important; background: #E44D4B;
-                color: white; float: right;"
+                style="border-radius: 50px; margin: 0 !important; border: 2px solid #E44D4B; 
+                color: #E44D4B; float: right; background: white;"
               >
                 Hapus Anggota
               </v-btn>
@@ -40,45 +40,169 @@
         cols="12"
         sm="6"
       >
-        <v-text-field
-          v-model="name"
-          label="Nama"
-        />
-        <v-text-field
-          v-model="email"
-          label="Email"
-        />
-        <v-btn class="my-5 primary subtitle-2 text-none px-5 font-weight-bold" style="border-radius: 50px">
-          Tambah Anggota
-        </v-btn>
+        <v-alert v-if="error" type="error" class="mt-4">
+          {{ error }}
+        </v-alert>
+        <form @submit.prevent="attemptCreateMember">
+          <v-text-field
+            v-model="name"
+            label="Nama"
+          />
+          <v-text-field
+            v-model="email"
+            label="Email"
+          />
+          <v-btn class="my-5 primary subtitle-2 text-none px-5 font-weight-bold"
+          style="border-radius: 50px" type="submit" :loading="isCreating">
+            Tambah Anggota
+          </v-btn>
+        </form>
       </v-col>
     </div>
   </div>
 </template>
 
-<script>
-import Vue from 'vue';
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
 import ProfileField from '~/components/partials/Dashboard/ProfileField.vue';
+import { ApiError } from '~/api/base';
+import { CreateMemberStatus, GetCompetitionStatus ,DeleteMemberStatus, Member } from '~/api/competition';
 
-export default Vue.extend({
-  name: 'AnggotaTim',
-  components: { ProfileField },
-  data: () => ({
-    members: [
-      {
-        name: 'Nella Zabrina Pramata',
-        email: 'nella.zabrina@gmail.com'
-      },
-      {
-        name: 'Ahmad Fahmi',
-        email: 'ahmad.fahmi@gmail.com'
-      }
-    ],
-    name: '',
-    email: '',
-    maxCapacity: 3
-  })
-});
+interface QueryParameters {
+  continue?: string;
+}
+
+export default class RegisterTeam extends Vue {
+  error: string = '';
+  name: string = '';
+  email: string = '';
+  teamId: number = 0;
+  members: Array<Member> = [];
+  memberId: number = -999;
+  maxCapacity: number = 3;
+  isCreating: boolean = false;
+  isDeleting: boolean = false;
+  isLoading: boolean = true;
+
+  get id() {
+    // eslint-disable-next-line dot-notation
+    return this.$route.params['competition'];
+  }
+
+  get nextRoute(): string|undefined {
+    const queryParams = this.$route.query as QueryParameters;
+    return queryParams.continue;
+  }
+
+  mounted() {
+    // TO DO: replace teamId with variabel from API
+
+    this.$arkavidiaApi.competition.getTeam(this.teamId)
+      .then((res) => {
+        if (res.competition) {
+          if (res.competition.maxTeamMembers) {
+            this.maxCapacity = res.competition.maxTeamMembers;
+          }
+        }
+
+        if (res.teamMembers) {
+          this.members = res.teamMembers;
+        }
+
+        const redirectUrl = (this.nextRoute) ? this.nextRoute : '/dashboard/competition/' + this.id + 'anggota-tim';
+        this.$router.push(redirectUrl);
+      })
+      .catch((e) => {
+        if (e instanceof ApiError) {
+          if (e.errorCode === GetCompetitionStatus.ERROR) {
+            this.error = 'Gagal saat Mengambil Data Anggota';
+            return;
+          }
+
+          this.error = e.message;
+          return;
+        }
+
+        this.error = e.toString();
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  }
+
+  attemptRegister() {
+    if (!this.name) {
+      this.error = 'Nama anggota harus diisi';
+      return;
+    }
+
+    if (!this.email) {
+      this.error = 'E-mail anggota harus diisi';
+      return;
+    }
+
+    if (!this.validateEmail(this.email)) {
+      this.error = 'Alamat e-mail salah';
+      return;
+    }
+
+    this.isCreating = true;
+    this.error = '';
+    this.$arkavidiaApi.competition.registerMember(this.teamId, this.name, this.email)
+      .then(() => {
+        const redirectUrl = (this.nextRoute) ? this.nextRoute : '/dashboard/competition/' + this.id + 'anggota-tim';
+        this.$router.push(redirectUrl);
+      })
+      .catch((e) => {
+        if (e instanceof ApiError) {
+          if (e.errorCode === CreateMemberStatus.EMAIL_EXISTS) {
+            this.error = 'E-mail sudah terdaftar';
+            return;
+          }
+
+          this.error = e.message;
+          return;
+        }
+
+        this.error = e.toString();
+      })
+      .finally(() => {
+        this.isCreating = false;
+      });
+  }
+
+  validateEmail(email): boolean {
+    // eslint-disable-next-line no-useless-escape
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  attemptDelete() {
+    this.isDeleting = true;
+    this.error = '';
+    this.$arkavidiaApi.competition.DeleteMember(this.teamId, this.memberId)
+      .then(() => {
+        const redirectUrl = (this.nextRoute) ? this.nextRoute : '/dashboard/competition/' + this.id + 'anggota-tim';
+        this.$router.push(redirectUrl);
+      })
+      .catch((e) => {
+        if (e instanceof ApiError) {
+          if (e.errorCode === DeleteMemberStatus.ERROR) {
+            this.error = 'Penghapusan Anggota Gagal';
+            return;
+          }
+
+          this.error = e.message;
+          return;
+        }
+
+        this.error = e.toString();
+      })
+      .finally(() => {
+        this.isDeleting = false;
+      });
+  }
+}
 </script>
 
 <style scoped>
