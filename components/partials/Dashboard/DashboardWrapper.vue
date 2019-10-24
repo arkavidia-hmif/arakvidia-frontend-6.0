897fc4caa5c81e442499f6e17dc141d0ed2d9295
Nella Zabrina <nellazabrinapramata@yahoo.co.id>
2019-10-24 18:31:37 +0700

    fixing prev padding tab

--- components/partials/Dashboard/DashboardWrapper.vue
+++ components/partials/Dashboard/DashboardWrapper.vue
@@ -7,7 +7,7 @@
       <div class="display-1 font-weight-bold mt-2 mb-5" style="color: #0B909A">
         Halo, {{ user.fullName }}!
       </div>
-      <v-tabs slider-color="#E44D4B" slider-size="5">
+      <v-tabs slider-color="#E44D4B" slider-size="5" class="noprevpadding">
         <v-tab v-for="(item, i) in dashboardMenus" :key="i" :to="item.route" :disabled="item.disabled || false" class="font-weight-bold black--text sub-title-1 text-none">
           {{ item.title }}
         </v-tab>
@@ -55,4 +55,7 @@ export default class DashboardWrapper extends Vue {
 </script>
 
 <style scoped>
+  .noprevpadding /deep/.v-slide-group__prev {
+    display: none !important;
+  }
 </style>
