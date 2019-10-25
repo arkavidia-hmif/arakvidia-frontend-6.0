8a93db14fa71dd1ecea3be593b783e745e74fd65
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-10-25 16:52:21 +0700

    Deploy informational pages to production. DO NOT MERGE THIS TO DEV!!!

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,8 +26,7 @@ export default {
   plugins: [
     '~/plugins/typedjs',
     '~/plugins/definitions',
-    { src: '~/plugins/vuex-persist', ssr: false },
-    { src: '~/plugins/arkavidia-api', ssr: false }
+    { src: '~/plugins/vuex-persist', ssr: false }
   ],
   buildModules: [
     '@nuxt/typescript-build',
