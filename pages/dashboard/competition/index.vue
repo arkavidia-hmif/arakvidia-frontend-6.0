32a0c6950b0fd6ebcf7deb47e646db125e0d981e
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-10-27 22:46:38 +0700

    Fix competition selection screen

--- pages/dashboard/competition/index.vue
+++ pages/dashboard/competition/index.vue
@@ -4,10 +4,10 @@
       <v-col v-for="competition in competitions" :key="competition.id" :cols="12" :lg="4" :sm="6">
         <CompetitionCard
           :title="competition.name"
-          subtitle=""
+          :subtitle="competitionsMap[competition.slug] ? competitionsMap[competition.slug]['name'] : ''"
           :to="'/dashboard/competition/' + competition.slug"
-          button-title="Daftar"
-          :enabled="true"
+          :button-title="competitionsMap[competition.slug] ? 'Lihat Pendaftaran' : 'Daftar'"
+          :enabled="competition.isRegistrationOpen"
         />
       </v-col>
     </v-row>
@@ -18,6 +18,7 @@
 import { Component, Action, Getter, Vue } from 'nuxt-property-decorator';
 import DashboardWrapper from '~/components/partials/Dashboard/DashboardWrapper.vue';
 import CompetitionCard from '~/components/partials/Dashboard/CompetitionCard.vue';
+import { Team } from '~/api/competition/types';
 
 @Component({
   components: { DashboardWrapper, CompetitionCard }
@@ -29,7 +30,11 @@ export default class DashboardIndex extends Vue {
     };
   }
   @Action('competition/fetchCompetitionList') fetchCompetitionListAction;
+  @Action('competition/fetchTeamList') fetchTeamListAction;
   @Getter('competition/getCompetitions') getCompetitions;
+  @Getter('competition/getTeams') getTeams;
+
+  competitionsMap = {};
 
   get competitions() {
     return this.getCompetitions;
@@ -37,6 +42,16 @@ export default class DashboardIndex extends Vue {
 
   mounted() {
     this.fetchCompetitionListAction();
+    this.fetchTeamListAction()
+      .then((teams: Team[]) => {
+        teams.forEach((team: Team) => {
+          if (!team.competition) { return; }
+
+          const competitionsMap = { ...this.competitionsMap };
+          competitionsMap[team.competition.slug] = team;
+          this.competitionsMap = competitionsMap;
+        });
+      });
   }
 }
 </script>
