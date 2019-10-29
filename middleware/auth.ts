1511bce4d49654a6c6fd4c0c06e2c8f17ec64c98
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-10-30 01:30:52 +0700

    Temporary, quick fix

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,8 @@
-export default function({ store, redirect, route }) {
+export default function({ redirect, route }) {
   // eslint-disable-next-line dot-notation
   if (!process['server']) {
     if (route.path.startsWith('/dashboard')) {
-      const exp = parseInt(window.localStorage.getItem("arkav-token-exp") || '0');
-      console.log('Exp', exp);
+      const exp = parseInt(window.localStorage.getItem('arkav-token-exp') || '0');
       if (Date.now() > exp) {
         redirect(`/login?continue=${encodeURIComponent(route.path)}`);
       }
