efdf2482ddedccd4ad2bb9f2ea4dfa5d5830f7e9
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-10-30 01:23:32 +0700

    Attempt to fix

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,7 +2,9 @@ export default function({ store, redirect, route }) {
   // eslint-disable-next-line dot-notation
   if (!process['server']) {
     if (route.path.startsWith('/dashboard')) {
-      if (!store.getters['user/isLoggedIn']) {
+      const exp = parseInt(window.localStorage.getItem("arkav-token-exp") || '0');
+      console.log('Exp', exp);
+      if (Date.now() > exp) {
         redirect(`/login?continue=${encodeURIComponent(route.path)}`);
       }
     }
