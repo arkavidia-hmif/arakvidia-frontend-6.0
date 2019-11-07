82bc20185dc2c17edfe8297c73c05af6f4b2cd65
didithilmy <didithilmy@gmail.com>
2019-11-07 10:06:38 +0700

    Remove secure checks

--- plugins/persisted-state.ts
+++ plugins/persisted-state.ts
@@ -2,14 +2,14 @@ import createPersistedState from 'vuex-persistedstate';
 import * as Cookies from 'js-cookie';
 import cookie from 'cookie';
 
-export default ({ store, req, isDev }) => {
+export default ({ store, req }) => {
   createPersistedState({
     key: 'arkavcookie',
     storage: {
       // eslint-disable-next-line dot-notation
       getItem: key => process['client'] ? Cookies.getJSON(key) : cookie.parse(req.headers.cookie || '')[key],
       setItem: (key, value) => {
-        Cookies.set(key, value, { expires: 365, secure: !isDev });
+        Cookies.set(key, value, { expires: 365 });
       },
       removeItem: key => Cookies.remove(key)
     }
