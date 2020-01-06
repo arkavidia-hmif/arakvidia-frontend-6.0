85781d19fc1ee1c06c4f9fa9de27b50980cbc323
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2020-01-06 20:19:43 +0700

    Add IceHouse

--- components/partials/CompanyLogos.vue
+++ components/partials/CompanyLogos.vue
@@ -4,7 +4,7 @@
     <v-container class="py-10">
       <div align="center" />
       <v-row>
-        <v-col :cols="6" :sm="4" :md="2">
+        <v-col :cols="6" :sm="4" :md="2" :offset-md="1">
           <div class="subtitle font-weight-bold">
             Technology Partner:
           </div>
@@ -14,7 +14,7 @@
             </a>
           </div>
         </v-col>
-        <v-col :cols="12" :md="9" :offset-md="1">
+        <v-col :cols="12" :md="7" :offset-md="1">
           <div class="subtitle font-weight-bold">
             Sponsored by:
           </div>
@@ -28,6 +28,9 @@
             <a href="https://mekari.com" target="_blank">
               <v-img class="d-inline-block ma-2" style="vertical-align: middle;" :src="require('~/assets/images/logo/sponsors/mekari.png')" width="170" />
             </a>
+            <a href="https://icehousecorp.com" target="_blank">
+              <v-img class="d-inline-block ma-2" style="vertical-align: middle;" :src="require('~/assets/images/logo/sponsors/icehouse.jpg')" width="180" />
+            </a>
             <a href="https://matagi.id" target="_blank">
               <v-img class="d-inline-block ma-2" style="vertical-align: middle;" :src="require('~/assets/images/logo/sponsors/matagi.png')" width="100" />
             </a>
@@ -39,7 +42,7 @@
         </v-col>
       </v-row>
       <v-row>
-        <v-col :cols="6" :sm="4" :md="2">
+        <v-col :cols="6" :sm="4" :md="2" :offset-md="1">
           <div class="subtitle font-weight-bold">
             Supported by:
           </div>
@@ -55,9 +58,6 @@
             <v-img :src="require('~/assets/images/logo/medpart_footer.png')" width="100%" />
             <v-img :src="require('~/assets/images/logo/medpart_footer_2.png')" width="100%" />
           </div>
-        </v-col>
-        <v-col :cols="12" :md="2">
-&nbsp;
         </v-col>
       </v-row>
     </v-container>
